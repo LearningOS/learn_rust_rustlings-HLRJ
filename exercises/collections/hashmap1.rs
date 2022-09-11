6610551f7f336a494a6d8248a79a687f8f1cd618
Zhenxiang Wang <befun@vip.qq.com>
2022-09-11 09:31:27 +0000

    update classroom.yml .keep autograding.json for classroom CI test

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,18 +11,21 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
+// 
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
-
+    basket.insert(String::from("apple"), 2);
+    basket.insert(String::from("mango"), 2);
+    basket.insert(String::from("zhi yin"), 2);
+    basket.insert(String::from("pdpdp"), 2);
     basket
 }
 
--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -8,7 +8,7 @@
 
 // Make these tests pass! Execute `rustlings hint errors6` for hints :)
 
-// I AM NOT DONE
+// 
 
 use std::num::ParseIntError;
 
@@ -21,6 +21,12 @@ enum ParsePosNonzeroError {
 
 impl ParsePosNonzeroError {
     // TODO: add another error conversion function here.
+    fn from_creation(err: CreationError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::Creation(err)
+    }
+    fn from_parse_int(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str)
@@ -28,9 +34,17 @@ fn parse_pos_nonzero(s: &str)
 {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
-    PositiveNonzeroInteger::new(x)
-        .map_err(ParsePosNonzeroError::from_creation)
+    let x  = s.parse();
+    match x {
+        Ok(x) => {
+            PositiveNonzeroInteger::new(x)
+            .map_err(ParsePosNonzeroError::from_creation)
+        }
+        Err(e) => {
+            Err(ParsePosNonzeroError::from_parse_int(e))
+        }
+    }
+   
 }
 
 // Don't change anything below this line.
